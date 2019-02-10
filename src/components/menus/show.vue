0b07db99ce1deb36db1a54a53632bc7e58e9558e
Meltens <irimamekun@gmail.com>
2019-02-11 00:31:20 +0900

    材料一覧をテーブルに変更

--- src/components/menus/show.vue
+++ src/components/menus/show.vue
@@ -1,11 +1,20 @@
 <template>
   <div>
-    <h2>{{ menu.name }}の材料</h2>
-    <div v-for="ingredient in ingredients" v-bind:key="ingredient.name">
-      <span>{{ ingredient.name }}</span><br>
-      <span>{{ ingredient.amount }}{{ ingredient.unit }}/人</span><br>
-      <span>{{ ingredient.cost }}円/人</span>
-    </div>
+    <table border="1" width="80%" aligen="center">
+      <caption>{{ menu.name }}の材料</caption>
+      <tr>
+        <th>材料名</th>
+        <th>量</th>
+        <th>金額</th>
+        <th>説明</th>
+      </tr>
+      <tr  v-for="ingredient in ingredients" v-bind:key="ingredient.name">
+        <td>{{ ingredient.name }}</td>
+        <td>{{ ingredient.amount }}{{ ingredient.unit }}/人</td>
+        <td>{{ ingredient.cost }}円/人</td>
+        <td>{{ ingredient.description }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
