bd57705c15d823c97610eda8f74015be4d3f826b
Gin.Furuya <irimamekun@gmail.com>
2019-02-13 13:09:49 +0900

    Install awsome-font (#8)
    * install awesome font

--- src/main.js
+++ src/main.js
@@ -5,6 +5,13 @@ import App from './App'
 import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(fas)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
