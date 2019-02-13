1807b1c0f1b253dc91fdd3e8f251dc4b35e2eda2
Meltens <irimamekun@gmail.com>
2019-02-13 16:52:57 +0900

    move Genres.vue @/components/genres/Index.vue

--- src/components/ManagementIndex.vue
+++ src/components/ManagementIndex.vue
@@ -5,6 +5,7 @@
       <li><router-link to="/genres">メニュー一覧</router-link></li>
       <li><router-link to="/users">ユーザー管理</router-link></li>
       <li><router-link to="/menu/new">メニュー追加</router-link></li>
+      <li><router-link to="/genre/new">ジャンル追加</router-link></li>
     </ul>
   </div>
 </template>
