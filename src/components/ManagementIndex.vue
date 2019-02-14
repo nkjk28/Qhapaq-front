98288815517fee3fe8daa55df264f1957d3fab67
Meltens <irimamekun@gmail.com>
2019-02-14 15:28:20 +0900

    ManagementIndexのRouterLinkをpathからnameに変更

--- src/components/ManagementIndex.vue
+++ src/components/ManagementIndex.vue
@@ -2,10 +2,10 @@
   <div>
     <h1>Management Page</h1>
     <ul>
-      <li><router-link to="/genres">メニュー一覧</router-link></li>
-      <li><router-link to="/users">ユーザー管理</router-link></li>
-      <li><router-link to="/menu/new">メニュー追加</router-link></li>
-      <li><router-link to="/genre/new">ジャンル追加</router-link></li>
+      <li><router-link :to="{name: 'Genres'}">メニュー一覧</router-link></li>
+      <li><router-link :to="{name: 'MenuNew'}">メニュー追加</router-link></li>
+      <li><router-link :to="{name: 'Users'}">ユーザー管理</router-link></li>
+      <li><router-link :to="{name: 'GenreNew'}">ジャンル追加</router-link></li>
     </ul>
   </div>
 </template>
