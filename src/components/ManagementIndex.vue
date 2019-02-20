50513b5b99802b1a16117b4a62226eea11f8e31d
karen1019 <zx323443@jk9.so-net.ne.jp>
2019-02-20 14:46:28 +0900

    ジャンル別メニューのデザイン変更

--- src/components/ManagementIndex.vue
+++ src/components/ManagementIndex.vue
@@ -1,10 +1,9 @@
 <template>
   <div>
     <router-link :to="{name: 'MenusIndex'}" tag="button" class="menu">メニュー<br>一覧 <font-awesome-icon icon="book-open" /></router-link>
-    <router-link :to="{name: 'Genres'}" tag="button" class="genre_menu">ジャンル別<br>メニュー <font-awesome-icon icon="book-open" /></router-link>
-    <router-link :to="{name: 'Users'}" tag="button" class="user_management">ユーザー<br>管理 <font-awesome-icon icon="user" /></router-link>
+    <router-link :to="{name: 'Genres'}" tag="button" class="genre_menu">ジャンル別<br>メニュー <font-awesome-icon icon="clipboard-list" /></router-link>
     <router-link :to="{name: 'MenuNew'}" tag="button" class="add_menu">メニュー<br>追加 <font-awesome-icon icon="plus" /></router-link>
-    <router-link :to="{name: 'GenreNew'}" tag="button" class="add_genre">ジャンル<br>追加 <font-awesome-icon icon="plus" /></router-link>
+    <router-link :to="{name: 'GenreNew'}" tag="button" class="add_genre">ジャンル<br>追加 <font-awesome-icon icon="plus-circle" /></router-link>
   </div>
 </template>
 
@@ -19,66 +18,40 @@ export default {
 
 <style>
 .menu, .genre_menu, .user_management, .add_menu, .add_genre{
-  position: absolute;
+  position: relative;
   width: 35vw;
   height: 35vh;
   font-size:5vh;
-  border-radius: 5vh;
-  box-shadow:1vh 1vw #555555;
+  border-radius: 2vw;
+  box-shadow:1vh 1vw #a1a1a1;
   -webkit-text-stroke: 1px #cccccc;
   font-weight: bold;
-  font-size: 7vh;
+  font-size: 5vw;
+  color: #424242;
+  background-color: #a2d0d8;
 }
 .menu{
-  top: 10vh;
-  left: 10vw;
-  background-color: #38d140;
+  top: 0vh;
+  left: 0vw;
 }
 
-.menu:hover{
-  background-color: #38a140;
+.menu:hover, .genre_menu:hover, .user_management:hover, .add_menu:hover, .add_genre:hover{
+  background-color: #13a9bd;
 }
 
 .genre_menu{
-  top: 100vh;
+  top: 0vh;
   left: 10vw;
-  top: 10vh;
-  left: 55vw;
-  background-color: #38d140;
-}
-
-.genre_menu:hover{
-  background-color: #38a140;
-}
-
-.user_management{
-  top: 100vh;
-  left: 10vw;
-  background-color: #ca4667;
-}
-
-.user_management:hover{
-  background-color: #ca2667;
 }
 
 .add_menu{
-  top: 55vh;
-  left: 10vw;
-  background-color: #d1ad38;
-}
-
-.add_menu:hover{
-  background-color: #d18d38;
+  top: 10vh;
+  left: 0vw;
 }
 
 .add_genre{
-  top: 55vh;
-  left: 55vw;
-  background-color: #4238d1;
-}
-
-.add_genre:hover{
-  background-color: #4218d1;
+  top: 10vh;
+  left: 10vw;
 }
 </style>
 >>>>>>> develop
